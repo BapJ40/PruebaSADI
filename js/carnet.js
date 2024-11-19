f27ec19c9006eebc17376b3966b4d7089c511d06
Admin <gjr776@gmail.com>
2024-11-18 22:38:38 -0400

    Se agregaron nuevos carnets

--- js/carnet.js
+++ js/carnet.js
@@ -7,13 +7,15 @@ class Carnet {
 
 const carnets = [];
 
-const carnet1 = new Carnet("assets/FUERZA MOTORIZADA MIRANDA CRISTO VIVE.jpg", "ACTIVO");
+const carnet1 = new Carnet("assets/carnet.jpg", "ACTIVO");
 carnets.push(carnet1);
-const carnet2 = new Carnet("assets/FUERZA MOTORIZADA MIRANDA CRISTO VIVE.jpg", "INACTIVO");
+const carnet2 = new Carnet("assets/carnet 2.jpg", "INACTIVO");
 carnets.push(carnet2);
-const carnet3 = new Carnet("assets/FUERZA MOTORIZADA MIRANDA CRISTO VIVE.jpg", "ACTIVO");
+const carnet3 = new Carnet("assets/carnet 3.jpg", "ACTIVO");
 carnets.push(carnet3);
-const carnet4 = new Carnet("assets/FUERZA MOTORIZADA MIRANDA CRISTO VIVE.jpg", "ACTIVO");
+const carnet4 = new Carnet("assets/carnet 4.jpg", "ACTIVO");
 carnets.push(carnet4);
+const carnet5 = new Carnet("assets/carnet 5.jpg", "ACTIVO");
+carnets.push(carnet5);
 
 export default carnets
